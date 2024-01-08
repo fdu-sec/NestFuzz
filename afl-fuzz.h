3c4fd650eb9391fea8f79c2496840f1e9b5e88bd
Me3h0n <me3h0n@gmail.com>
2024-01-08 10:10:49 +0800

    fix: afl compile error when gcc version >= 10

--- afl-fuzz.h
+++ afl-fuzz.h
@@ -265,12 +265,12 @@ struct queue_entry {
       *next_100;            /* 100 elements ahead               */
 };
 
-struct queue_entry *queue, /* Fuzzing queue (linked list)      */
+extern struct queue_entry *queue, /* Fuzzing queue (linked list)      */
     *queue_cur,                   /* Current offset within the queue  */
     *queue_top,                   /* Top of the list                  */
     *q_prev100;                   /* Previous 100 marker              */
 
-struct queue_entry*
+extern struct queue_entry*
     top_rated[MAP_SIZE]; /* Top entries for bitmap bytes     */
 
 struct extra_data {
